f42bd34d3a4de12f7f1e93da9ebe3e59a1329651
pabs <devnull@localhost>
2003-09-02 00:01:22 +0000

    * Mon Sep 01 20:01:08 2003, pabs <pabs@pablotron.org>   * added web/config.php

---
+++ web/config.php
@@ -0,0 +1,20 @@
+<?php
+
+#########################################################################
+# config.php - localfark web interface config file                      #
+#########################################################################
+
+# show the forum?
+$SHOW_FORUM_LINK = false;
+
+# database options
+$DB_OPTS = array(
+  'user'  => 'USER',
+  'pass'  => 'PASS',
+  'host'  => 'localhost',
+  'db'    => 'DB',
+  'table' => 'TABLE',
+);
+
+?>
+
